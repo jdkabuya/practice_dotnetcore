61fa0a455eede6979a1386cbc07c74532d3aa005
Justin Dibula <jdkabuya@yahoo.com>
2024-04-23 21:16:56 +0200

    end third lesson

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// this is to fix the error that is happening on the server
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy",policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    }) ;   
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
